6085e931fedf753754fbf432f625c96b9ecea6ed
Caleb Spindler <caleb.spindler@gmail.com>
2020-09-22 12:29:11 -0700

    initial commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,12 @@
 import React from "react"
-import {Link} from "gatsby"
-import { RiHeart2Line } from "react-icons/ri";
+// import {Link} from "gatsby"
+// import { RiHeart2Line } from "react-icons/ri";
 
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>A GatsbyJS Starter for Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Stackrole.com</Link></p>
+      <p>&#169; 2020 Spindler Ceramics</p>
+      {/* <p>A GatsbyJS Starter for Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Stackrole.com</Link></p> */}
     </div>
   </footer>
 )
--- src/content/pages/contact.md
+++ src/content/pages/contact.md
@@ -5,4 +5,4 @@ title: Get in touch
 ---
 Got a burning question regarding **pottery**? Need a new **coffee mug**? Or just wanna chat. We'd love to connect!
 
-Send us a message using the form below, or you can send us a DM on [Instagram](https://instagram.com/spindlerceramics)
+Send us a message using the form below, or you can send us a DM on [Instagram](https://instagram.com/spindlerceramics).
